885390906c1e465fe3d61140288f2e8f1efa4c0d
Abdullah Al Khalid <khalidrafi1111@gmail.com>
2024-10-15 14:33:24 +0800

    added typescript support for server.ts

---
+++ server/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+import {router as authsRouter}  from './routes/auths';
+app.use('/auths', authsRouter);
+
+// Define the server port
+const PORT: number = 4444;
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
