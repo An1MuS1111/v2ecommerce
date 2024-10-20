03f722244d1b64744045ed8c319df93858602801
Abdullah Al Khalid <khalidrafi1111@gmail.com>
2024-10-20 21:54:03 +0800

    frontend created

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+// import AuthProvider from "./auth/AuthProvider";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import AppRoutes from "./routes/AppRoutes";
+
+function App() {
+    return (
+        // <AuthProvider>
+        //     <Router>
+        //         <AppRoutes />
+        //     </Router>
+        // </AuthProvider>
+
+        <>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </>
+    );
+}
+
+export default App;
