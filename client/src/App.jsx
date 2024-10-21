accf7de0f3d50aa3e57d47da57e2ab8e2635fa7c
Abdullah Al Khalid <khalidrafi1111@gmail.com>
2024-10-21 11:33:08 +0800

    completed some fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
 import "./App.css";
 import TableLayout from "./views/components/TableLayout";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Loader from "./common/Loader";
 
 // import AuthProvider from "./auth/AuthProvider";
 // import { BrowserRouter as Router } from "react-router-dom";
 // import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-    return (
+    const [loading, setLoading] = useState(true);
+    // const { pathname } = useLocation();
+
+    // useEffect(() => {
+    //     window.scrollTo(0, 0);
+    // }, [pathname]);
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 1000);
+    }, []);
+
+    return loading ? (
+        <Loader />
+    ) : (
         // <AuthProvider>
         //     <Router>
         //         <AppRoutes />
--- client/src/components/TableLayout.jsx
+++ client/src/components/TableLayout.jsx
@@ -37,30 +37,6 @@ const TableLayout = () => {
                         </th>
                     </tr>
                 </thead>
-
-                {/* rest of the rows */}
-                {/* <tbody>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th
-                            scope="row"
-                            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                        >
-                            Apple MacBook Pro 17"
-                        </th>
-                        <td className="px-6 py-4">Silver</td>
-                        <td className="px-6 py-4">Laptop</td>
-                        <td className="px-6 py-4">$2999</td>
-                        <td className="px-6 py-4 text-right">
-                            <a
-                                href="#"
-                                className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                            >
-                                Edit
-                            </a>
-                        </td>
-                    </tr>
-                </tbody> */}
-
                 <tbody>
                     {data.map((item) => (
                         <tr
@@ -68,7 +44,10 @@ const TableLayout = () => {
                             className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
                         >
                             {headers.map((key) => (
-                                <td key={key} className="px-6 py-4">
+                                <td
+                                    key={key}
+                                    className="px-6 py-4 text-ellipsis  overflow-hidden max-w-xs text-nowrap"
+                                >
                                     {key === "is_admin"
                                         ? item[key]
                                             ? "Admin"
